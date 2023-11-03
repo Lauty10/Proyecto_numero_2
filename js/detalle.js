804b9ce958145720de2e611d6bf1f9a4519cf629
Lautaro Maldonado <lautylty@hotmail.com>
2023-11-03 17:18:20 -0300

    cambios

--- js/detalle.js
+++ js/detalle.js
@@ -5,17 +5,21 @@ const ubicationData = JSON.parse(localStorage.getItem("localProducts"));
 const filterUbication = ubicationData.filter((fb) => fb.id == Number(idProd));
 
 divMap.innerHTML = filterUbication.map((detalle) => {
-
   return `
-  <div id="product-container" class="d-flex justify-content-center align-items-center flex-column">
-  <div class="card" style="width: 18rem;">
-  <img src="${detalle.imagen}" class="card-img-top" alt="producto">
-  <div class="card-body">
-    <h5 class="card-title">${detalle.nombre}</h5>
-    <p class="card-text">${detalle.descripcion}</p>
-    <a href="#" class="btn btn-primary">Añadir al carrito</a>
-  </div>
-</div>
-</div>
+  <div class="d-flex">
+  <img 
+      src="${detalle.imagen}"
+      class="card-img-top class-img"
+      alt="..."
+      />
+      <div class="card-body">
+      <h5 class="card-title">${detalle.nombre}</h5>
+      <p class="card-text">
+      ${detalle.descripcion}
+      </p>
+      <button class='btn btn-warning' onclick="addCart(${detalle.id})">Agregar al carrito</button>
+      </div>
+      </div>
+
   `;
 });
