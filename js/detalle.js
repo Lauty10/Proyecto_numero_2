daf925093a926714d15a267a1a721b3c59451e8c
unknown <alvarosgd97@gmail.com>
2023-11-03 02:23:06 -0300

    detalles

--- js/detalle.js
+++ js/detalle.js
@@ -1,28 +1,29 @@
-const divMap = document.getElementById('prodVista')
-const idProd = location.search.split('=')[1]
+const divMap = document.getElementById("prodVista");
+const idProd = location.search.split("=")[1];
 console.log(idProd);
-const ubicationData = JSON.parse(localStorage.getItem('localProducts'))
-const filterUbication = ubicationData.filter((fb)=> fb.id==Number(idProd))
+const ubicationData = JSON.parse(localStorage.getItem("localProducts"));
+const filterUbication = ubicationData.filter((fb) => fb.id == Number(idProd));
 
-
-divMap.innerHTML = filterUbication
-
-    .map((detalle) => {
-        const cardId = `card-${detalle.id}`;
-        return `
-            <div class="d-flex">
-            <img 
+divMap.innerHTML = filterUbication.map((detalle) => {
+    return `
+      <div class="container">
+        <div class="product-card">
+          <img 
             src="${detalle.imagen}" 
             class="card-img-top class-img" 
             alt="producto" 
-            />
-            <div class="card-body">
+            style="width: 50%;"  
+          />
+          <div class="card-body">
             <h5 class="card-title">${detalle.nombre}</h5>
             <p class="card-text">
-                ${detalle.descripcion}
-                </p>
-                </div>
-                </div>
+              ${detalle.descripcion}
+            </p>
+          </div>
+        </div>
+      </div>
     `;
-    })
-    
+  });
+  
+  
+  
