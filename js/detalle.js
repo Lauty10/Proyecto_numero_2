7a879a903a4c2269a5ca8a33e82efd96d3ce50f5
unknown <alvarosgd97@gmail.com>
2023-11-06 22:08:57 -0300

    rutas priv

--- js/detalle.js
+++ js/detalle.js
@@ -15,6 +15,7 @@ divMap.innerHTML = `
           ${detalle.verso}
         </p>
         <button class='btn btn-warning mt-5 mb-5 me-5 w-20' onclick="addCart(${detalle.id})">Agregar al carrito</button>
+        <button class='btn btn-warning mt-5 mb-5 me-5 w-20' href="../html/error404.html";">Pagar Producto</button>
         <img src="${detalle.imagen}" alt="Descripción de la imagen" class="imagen-tienda"  style="box-shadow: 10px 10px 10px rgba(255, 255, 0, 0.7); border: 10px solid rgba(255, 255, 0, 0,9);width: 200px; height: 200px;"">
       </div>
       <p class="card-text mt-5" style="color: white; font-family: 'Kenia', sans-serif; font-size: 45px;">
